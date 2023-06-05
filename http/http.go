16265b9235ec56d504230bc67f38a187ac057daf
qicz <qiczzhu@gmail.com>
2023-06-05 20:02:07 +0800

    add reflect convert

--- http/http.go
+++ http/http.go
@@ -38,14 +38,13 @@ func InitEngine(cfg *settings.Setting, writer io.Writer) *gin.Engine {
 	router.Use(gin.LoggerWithConfig(gin.LoggerConfig{
 		Formatter: func(param gin.LogFormatterParams) string {
 			// custom format
-			return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s %s \"%s\" %s\"\n",
+			return fmt.Sprintf("%s - [%s] \"%s %s %s %d %s \"%s\" %s\"\n",
 				param.ClientIP,
 				param.TimeStamp.Format(time.RFC1123),
 				param.Method,
 				param.Path,
 				param.Request.Proto,
 				param.StatusCode,
-				// param.Keys[constant.RequestId],
 				param.Latency,
 				param.Request.UserAgent(),
 				param.ErrorMessage,
