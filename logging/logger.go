d329ad75e37c3050b75980be34c71d28b59a8b0c
qi <qiczzhu@gmail.com>
2023-07-06 15:24:26 +0800

    Refactor (#6)
    * update readme
    
    * mv ti structcp
    
    * update
    
    ---------
    
    Co-authored-by: zhucongqi <zhucongqi@cestc.cn>

--- logging/logger.go
+++ logging/logger.go
@@ -1,55 +1,70 @@
-/*
- * Licensed to the AcmeStack under one or more contributor license
- * agreements. See the NOTICE file distributed with this work for
- * additional information regarding copyright ownership.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *   http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package logging
 
 import (
-	"io"
 	"os"
-	
+
+	"github.com/go-logr/logr"
+	"github.com/go-logr/zapr"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
 )
 
-var (
-	Logger *zap.SugaredLogger
-)
-
-func init() {
-	InitLogger("info")
+// Logger 's Sugar A Sugar wraps the base Logger functionality in a slower, but less
+// verbose, API. Any Logger can be converted to a SugaredLogger with its Sugar
+// method.
+//
+// Unlike the Logger, the SugaredLogger doesn't insist on structured logging.
+// For each log level, it exposes four methods:
+//
+//   - methods named after the log level for log.Print-style logging
+//   - methods ending in "w" for loosely-typed structured logging
+//   - methods ending in "f" for log.Printf-style logging
+//   - methods ending in "ln" for log.Println-style logging
+//
+// For example, the methods for InfoLevel are:
+//
+//	Info(...any)           Print-style logging
+//	Infow(...any)          Structured logging (read as "info with")
+//	Infof(string, ...any)  Printf-style logging
+//	Infoln(...any)         Println-style logging
+type Logger struct {
+	logr.Logger
+	level LogLevel
+	Sugar *zap.SugaredLogger
 }
 
-func InitLogger(level string) {
-	l, err := zapcore.ParseLevel(level)
-	if err != nil {
-		l = zapcore.InfoLevel
+func InitLogger(level LogLevel) Logger {
+	logger := initZapLogger(level)
+	return Logger{
+		Logger: zapr.NewLogger(logger),
+		Sugar:  logger.Sugar(),
 	}
-	
-	encoderConfig := zap.NewProductionEncoderConfig()
-	encoderConfig.EncodeTime = zapcore.RFC3339TimeEncoder
-	encoderConfig.EncodeLevel = zapcore.CapitalLevelEncoder
-	
-	encoder := zapcore.NewConsoleEncoder(encoderConfig)
-	core := zapcore.NewCore(encoder, zapcore.NewMultiWriteSyncer(zapcore.AddSync(os.Stdout)), zap.NewAtomicLevelAt(l))
-	zapLogger := zap.New(core, zap.AddCaller())
-	
-	Logger = zapLogger.Sugar()
 }
 
-func MultiWriter() io.Writer {
-	return io.MultiWriter(os.Stdout)
+// WithName returns a new Logger instance with the specified name element added
+// to the Logger's name.  Successive calls with WithName append additional
+// suffixes to the Logger's name.  It's strongly recommended that name segments
+// contain only letters, digits, and hyphens (see the package documentation for
+// more information).
+func (l Logger) WithName(name string) Logger {
+	logger := initZapLogger(l.level)
+
+	return Logger{
+		Logger: zapr.NewLogger(logger).WithName(name),
+		Sugar:  l.Sugar,
+	}
+}
+
+// WithValues returns a new Logger instance with additional key/value pairs.
+// See Info for documentation on how key/value pairs work.
+func (l Logger) WithValues(keysAndValues ...interface{}) Logger {
+	l.Logger = l.Logger.WithValues(keysAndValues...)
+	return l
+}
+
+func initZapLogger(level LogLevel) *zap.Logger {
+	parseLevel, _ := zapcore.ParseLevel(string(level))
+	core := zapcore.NewCore(zapcore.NewConsoleEncoder(zap.NewDevelopmentEncoderConfig()), zapcore.AddSync(os.Stdout), zap.NewAtomicLevelAt(parseLevel))
+
+	return zap.New(core, zap.AddCaller())
 }
